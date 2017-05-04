6f53ba13a51b0d54f9b2cb3fa61239f141c916c6
ethanryan <ethanryan@gmail.com>
2017-05-04 13:50:20 -0400

    serializer: PostAuthorSerializer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,14 +5,22 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  # def show
+  #   @post = Post.find(params[:id])
+  #   respond_to do |format|
+  #     format.html { render :show }
+  #     format.json { render json: @post.to_json(only: [:title, :description, :id],
+  #                             include: [author: { only: [:name]}]) }
+  #   end
+  # end
+
   def show
-    @post = Post.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}]) }
-    end
-  end
+   @post = Post.find(params[:id])
+   respond_to do |format|
+     format.html { render :show }
+     format.json { render json: @post}
+   end
+ end
 
   def new
     @post = Post.new
